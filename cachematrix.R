b23f0c31ccf280cebaf694a856d46e8f249c7798
aidan-mcG <63246118+aidan-mcG@users.noreply.github.com>
2020-05-04 12:14:09 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
 ## these functions work together to create a matrix, calculate its inverse, and cache this inverse
 
-## makeCacheMatrix creates the matrix and defines a list of variables
+## makeCacheMatrix creates the a matrix object, as well as several variables. I is a variable in which the invertet matrix will be stored. 
+## i must first be cleared in case a previous value was cached. 
+## set is a function which is responsible for solving the inverse and storing it
+## get is a function which is involved in retrieving the cached inverse 
 
 makeCacheMatrix <- function(x = matrix) {
       i <- NULL
@@ -16,7 +19,8 @@ makeCacheMatrix <- function(x = matrix) {
            getinverse = getinverse)
 }
 
-## cacheSolve calcultes the inverse if it is found to not already be cached
+## cacheSolve first calls on the get function and checks if a cached value is returned. 
+## i is then reset to be the new inverse. get retrieves the data, the inverse is calculated using solve, and the data is set and printed.
 
 cacheSolve <- function(x, ...) {
       i <- x$getinverse()
